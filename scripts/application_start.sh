035dfcb06d242372f58472d21de505371a1fe413
Yingqi YQ12 Huang <huangyq12@lenovo.com>
2023-04-12 14:46:57 +0800

    modify app directroy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/bigyellow/express-app
+sudo chmod -R 777 /Users/bigyellow/Documents/express-app
 
 #navigate into our working directory where we have all our github files
-cd /home/bigyellow/express-app
+cd /Users/bigyellow/Documents/express-app
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"
