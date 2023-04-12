ea93b75f0f4ca235f1a9670d0ad2c264f7a6bc13
Yingqi YQ12 Huang <huangyq12@lenovo.com>
2023-04-12 17:01:12 +0800

    test11

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+    res.send('<h1>Express Demo App</h1> <h4>Message1: Success</h4> <p>Version 1.1</p>');
 })
 app.get('/products', (req, res) => {
     res.send([{productId: '101', price: 100}, {productId: '102', price: 150}])
